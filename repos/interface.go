a3b5d83696122b01e69f46641c806a5139455cfd
viogami <2654613995@qq.com>
2024-02-28 14:11:10 +0800

    add fav

--- repos/interface.go
+++ repos/interface.go
@@ -4,6 +4,7 @@ import "github.com/viogami/FavAni/database"
 
 type Repository interface {
 	User() UserRepository
+	Fav() FavRepository
 }
 
 type Migrant interface {
@@ -19,4 +20,10 @@ type UserRepository interface {
 	Create(*database.User) (*database.User, error)
 	Update(*database.User) (*database.User, error)
 	Delete(user database.User) error
+}
+
+type FavRepository interface {
+	GetFav(username string) (database.Favs, error)
+	AddFav(username string, fav database.Fav) error
+	DeleteFav(username string, fav database.Fav) error
 }
